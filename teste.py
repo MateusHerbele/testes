8a9176de88c248bae0eb6d6285ae34e09a2ca239
Mateus Herbele <73367371+MateusHerbele@users.noreply.github.com>
2024-10-17 10:56:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,9 @@
+  GNU nano 7.2                                          teste.py                                                    
 import socket
 
 sck = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-sck.bind(("interface-name", 0)) 
+sck.bind(("172.17.0.2", 0)) 
 sck.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1) # Include IP headers
-sck.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON) # Promiscuous mode
 
 try:
     while True:
@@ -11,5 +11,4 @@ try:
 
 except KeyboardInterrupt:
     print("Terminating process...")
-    sck.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
     socket.close()
